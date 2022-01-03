429a74ed0970552c7c4fc4b1b4adfee884a87f8c
Rondell Jefferson <rondell804@gmail.com>
2022-01-03 12:12:56 -0600

    Finished Personalizing App

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   const year = new Date().getFullYear();
   return (
     <footer>
-      <p>Copyright ⓒ {year}</p>
+      <p>Rondell Jefferson Copyright ⓒ {year}</p>
     </footer>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Header() {
   return (
     <header>
-      <h1>Keeper</h1>
+      <h1>To-Do App</h1>
     </header>
   );
 }
